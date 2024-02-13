b93ef5b725804e1d7d285645b1f261b8a218b180
Nathsambhu2703 <121147435+Nathsambhu2703@users.noreply.github.com>
2024-02-13 10:28:30 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,16 @@
+let card = document.querySelectorAll(".trending");
+let blog = document.querySelector(".trend");
+
+let mainPage = document.querySelector(".main");
+
+function shops(){
+mainPage.style.display="none";
+blog.style.display="none";
+
+
+//card.style.display="none"
+} 
+
+function blogs(){
+mainPage.style.display="none";
+}
